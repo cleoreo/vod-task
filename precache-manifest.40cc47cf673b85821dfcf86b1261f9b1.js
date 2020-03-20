414cfbbae04b5037c555df10009a28bf0c53dc04
Cleo Chan <wah.cleo@gmail.com>
2020-03-20 12:26:19 +0800

    Updates

---
+++ precache-manifest.40cc47cf673b85821dfcf86b1261f9b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd5ae0991236576ca2c",
+    "url": "/css/bundle.e287f13.css"
+  },
+  {
+    "revision": "4fa6ee8fbf70089dc08b86bd0d1b7fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd5ae0991236576ca2c",
+    "url": "/scripts/bundle.e287f13.js"
+  },
+  {
+    "revision": "f7b53f864fa43c99378c93e31f94dd3b",
+    "url": "/scripts/bundle.e287f13.js.LICENSE"
+  },
+  {
+    "revision": "1c111495cd94ca62de78",
+    "url": "/scripts/modernizr.e287f13.js"
+  },
+  {
+    "revision": "98fe731926ca7c3ac473a550f124342a",
+    "url": "/scripts/modernizr.e287f13.js.LICENSE"
+  },
+  {
+    "revision": "3e0216a8a635c2aed765701e35802673",
+    "url": "/static/icon.e287f13.svg"
+  },
+  {
+    "revision": "8d362760b25279384e2466f9bd832ce8",
+    "url": "/static/react-redux-saga.e287f13.svg"
+  }
+]);
