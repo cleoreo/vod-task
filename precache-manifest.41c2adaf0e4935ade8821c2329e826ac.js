3fe8a2deb7a373a98833ec4cd7600f7bd201d220
Cleo Chan <wah.cleo@gmail.com>
2020-03-23 02:31:41 +0800

    Updates

---
+++ precache-manifest.41c2adaf0e4935ade8821c2329e826ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315d9afa00837c63aeea",
+    "url": "/vod-taskcss/bundle.a78f73a.css"
+  },
+  {
+    "revision": "f6fea8dc6efb3eb2102b9c46af2241e0",
+    "url": "/vod-taskindex.html"
+  },
+  {
+    "revision": "315d9afa00837c63aeea",
+    "url": "/vod-taskscripts/bundle.a78f73a.js"
+  },
+  {
+    "revision": "9eda218de2fdb238114e4d04d1a4f6f7",
+    "url": "/vod-taskscripts/bundle.a78f73a.js.LICENSE"
+  },
+  {
+    "revision": "061d170f54db443961ae",
+    "url": "/vod-taskscripts/modernizr.a78f73a.js"
+  },
+  {
+    "revision": "98fe731926ca7c3ac473a550f124342a",
+    "url": "/vod-taskscripts/modernizr.a78f73a.js.LICENSE"
+  },
+  {
+    "revision": "3e0216a8a635c2aed765701e35802673",
+    "url": "/vod-taskstatic/icon.a78f73a.svg"
+  },
+  {
+    "revision": "97c0fc2d4a756558239066c12eafe24f",
+    "url": "/vod-taskstatic/owl.video.play.png"
+  },
+  {
+    "revision": "8d362760b25279384e2466f9bd832ce8",
+    "url": "/vod-taskstatic/react-redux-saga.a78f73a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vod-taskprecache-manifest.7b66ca7014a49523f5fa1d9e66e31763.js"
+  "/vod-taskprecache-manifest.41c2adaf0e4935ade8821c2329e826ac.js"
 );
 
 self.addEventListener('message', (event) => {
